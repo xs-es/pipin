125f95688f481669e614f0cb02d594ed7d7de66e
Xses <xses.echo@gmail.com>
2025-05-14 00:15:19 -0400

    Update pipin.py

--- pipin.py
+++ pipin.py
@@ -5,4 +5,4 @@ def install_package(package_name):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
 
 # Example usage:
-install_package("requests")
+# install_package("requests")
